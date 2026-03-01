f7fe0a1e96063baa05e5fdb9708e05c11683db9f
akshatextreme <akshatextreme@gmail.com>
2026-03-01 16:37:55 +0400

    fix: point DemoApplicationTests at SaaSApplication, add dev profile

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.demoapplication.saas.SaaSApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = SaaSApplication.class)
+@ActiveProfiles("dev")
 class DemoApplicationTests {
 
 	@Test
